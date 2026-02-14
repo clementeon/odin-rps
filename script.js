d549b5e2a94292c9930f305294ec4f8fe006b7e4
Clement Zhou <clmementzhou2@gmail.com>
2026-02-13 23:07:35 -0500

    add GUI and update icons

--- script.js
+++ script.js
@@ -30,7 +30,8 @@ function playRound(computerChoice, humanChoice) {
     if (computerChoice == humanChoice) {
         return null;
     } else if ((computerChoice == rock && humanChoice == paper) 
-        ||(computerChoice == paper && humanChoice == scissor) || (computerChoice == scissor && humanChoice == rock)) {
+        ||(computerChoice == paper && humanChoice == scissor) 
+        || (computerChoice == scissor && humanChoice == rock)) {
         return true;
     } else {
         return false;
@@ -41,7 +42,8 @@ function playGame() {
     let compScore = 0;
     let humScore = 0;
     while (compScore < 5 && humScore < 5) {
-        console.log("computer score is " + compScore + " human score is " + humScore);
+        console.log("computer score is " + compScore + 
+                        " human score is " + humScore);
         let compval = getComputerChoice();
         console.log(compval);
         let humval = getHumanChoice();
