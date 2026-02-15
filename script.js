e7f552d387a51d6be4c60a9313e8b1f38a723ea1
Clement Zhou <clmementzhou2@gmail.com>
2026-02-14 21:27:04 -0500

    debug new button logic and end game state

--- script.js
+++ script.js
@@ -4,35 +4,34 @@ const paper = 1;
 const scissor = 2;
 
 const conversion = {
-    'rock' : 0,
-    'paper' : 1,
-    'scissor' : 2,
-}
+    rock: 0,
+    paper: 1,
+    scissor: 2,
+};
 
 const IMAGES = {
-  0 : "img/odin-rock.png",
-  1 : "img/odin-paper.png",
-  2 : "img/odin-scissor.png",
-  'default' : "img/odin-rps-placeholder.png",
-  'reset' : "img/restart-icon.png"
+    0: "img/odin-rock.png",
+    1: "img/odin-paper.png",
+    2: "img/odin-scissor.png",
+    default: "img/odin-rps-placeholder.png",
+    reset: "img/restart-icon.png",
 };
 const choices = document.querySelectorAll(".choice");
 
-const compVal = document.querySelector(".comp-choice")
+const compVal = document.querySelector(".comp-choice");
 compVal.disabled = true;
 
-const next = document.querySelector(".nexticon")
+const next = document.querySelector(".nexticon");
 next.disabled = true;
-const img = document.querySelector('.nexticon img');
-
+const img = document.querySelector(".nexticon img");
 
 const gamestate = {
-    'userChoice' : 0,
-    'compPlay' : 0,
-    'userWins' : 0,
-    'compWins' : 0,
-}
-
+    userChoice: 0,
+    compPlay: 0,
+    userWins: 0,
+    compWins: 0,
+    roundDone: false,
+};
 
 //User input and takes in a user value, ensuring only the selected value is
 //highlighted
@@ -40,27 +39,30 @@ const gamestate = {
 //this turn
 choices.forEach((choice) => {
     choice.addEventListener("click", () => {
-        compVal.disabled = false;
+        if (gamestate.roundDone) return;
 
+        compVal.disabled = false;
         gamestate.userChoice = conversion[choice.id];
         removeHighlight();
-    choice.style.outline = "3px solid #FFFFFF"
-  });
-})
+        choice.style.outline = "3px solid #FFFFFF";
+    });
+});
 
 //resets the game by setting all the gamestates to 0, as well as returning all
 // the imgaes to default
 // disables computer button and reset button
 function resetGame() {
     removeHighlight();
-    for(key in gamestate) {
+    for (key in gamestate) {
         gamestate[key] = 0;
     }
+    gamestate.roundDone = false;
+
     choices.forEach((choice) => {
         choice.style.outline = "none";
-    })
+    });
 
-    const results = document.querySelectorAll('.result');
+    const results = document.querySelectorAll(".result");
 
     results.forEach((result) => {
         result.style.backgroundColor = "";
@@ -69,23 +71,25 @@ function resetGame() {
         img.src = IMAGES.default;
         img.style.padding = "";
     });
+    choices.forEach((c) => (c.disabled = false));
     compVal.disabled = true;
     next.disabled = true;
-}
 
+}
 
 //Selects the computer choice and updates based off the choice
 //contains case for end of game where at 5 wins, all buttons but the reset
 //button are available
 compVal.addEventListener("click", () => {
-    gamestate.compChoice = (Math.floor((Math.random() * 10)) % 3);
-    let temp = document.querySelector('#comp-img');
-    let disc = document.querySelector('#comp-text');
+    if (gamestate.roundDone) return;
+
+    gamestate.compChoice = Math.floor(Math.random() * 10) % 3;
+    let temp = document.querySelector("#comp-img");
+    let disc = document.querySelector("#comp-text");
     if (gamestate.compChoice == 0) {
         temp.src = IMAGES[0];
         disc.textContent = `ROCK`;
-    }
-    else if (gamestate.compChoice == 1) {
+    } else if (gamestate.compChoice == 1) {
         temp.src = IMAGES[1];
         disc.textContent = `PAPER`;
     } else {
@@ -94,76 +98,51 @@ compVal.addEventListener("click", () => {
     }
     playRound(gamestate.userChoice, gamestate.compChoice);
     //at 5 wins, locks all butons except reset
-    // resets the images and the gamestate
+    // resets the images and the gamestate    
     if (gamestate.compWins == 5 || gamestate.userWins == 5) {
-        choices.disabled = true;
-        compVal.disabled = true;
+        gamestate.roundDone = true;
         next.disabled = false;
         removeHighlight();
-        next.style.outline = "3px solid #FFFFFF"
-
+        next.style.outline = "3px solid #FFFFFF";
     }
-})
+});
 
 //to go next or reset the game
 //toggle on user button and off comp and reset the highlighting when clicked
 //should always toggle itslef off at the end
-next.addEventListener('click', () => {
-    if (gamestate.compWins == 5 || gamestate.userWins == 5) {
-        resetGame();
-    } else {
-        removeHighlight()
-        choices.disabled = false;
-    }
-    next.disabled = true;
+next.addEventListener("click", resetGame);
 
-})
-
-
-//plays game and updates image for scores
 function playRound(humanChoice, computerChoice) {
     roundWin = (humanChoice - computerChoice + 3) % 3;
     if (roundWin == 1) {
         gamestate.userWins += 1;
         const img = document.querySelector(
-            `.human-results [data-index="${gamestate.userWins}"] img`
+            `.human-results [data-index="${gamestate.userWins}"] img`,
         );
         img.src = IMAGES[gamestate.userChoice];
-        img.style.padding =  "5px";
+        img.style.padding = "5px";
         const bg = document.querySelector(
-            `.human-results [data-index="${gamestate.userWins}"]`)
-        bg.style.backgroundColor = "#1E90FF"   
+            `.human-results [data-index="${gamestate.userWins}"]`,
+        );
+        bg.style.backgroundColor = "#1E90FF";
     } else if (roundWin == 2) {
-        console.log('computer won')
+        console.log("computer won");
         gamestate.compWins += 1;
         const img = document.querySelector(
-            `.comp-results [data-index="${gamestate.compWins}"] img`
+            `.comp-results [data-index="${gamestate.compWins}"] img`,
         );
         img.src = IMAGES[gamestate.compChoice];
-        img.style.padding =  "5px";
+        img.style.padding = "5px";
         const bg = document.querySelector(
-            `.comp-results [data-index="${gamestate.compWins}"]`)
-        bg.style.backgroundColor = "#E60000"
+            `.comp-results [data-index="${gamestate.compWins}"]`,
+        );
+        bg.style.backgroundColor = "#E60000";
     }
 }
 
 function removeHighlight() {
-    let highlighted = document.querySelectorAll('button');
+    let highlighted = document.querySelectorAll("button");
     highlighted.forEach((val) => {
         val.style.outline = "none";
-    })
+    });
 }
-// function highlightWinner() {
-//   const human = document.querySelector(".human-results");
-//   const computer = document.querySelector(".comp-results");
-
-//   human.classList.remove("winner-blink");
-//   computer.classList.remove("winner-blink");
-
-//   if (gamestate.userWins === 5) {
-//     human.classList.add("winner-blink");
-//   } else if (gamestate.compWins == 5) {
-//     computer.classList.add("winner-blink");
-//   }
-// }
-
